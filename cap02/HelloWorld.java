174fa229bac5b314f60794472b56dcb9839f4292
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2026-09-21 14:19:31 -0300

    add HelloWorld.java and Eco.java

---
+++ cap02/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld { // Deeclaração da classe HelloWorld
+	// Método principal de execução do programa
+	public static void main(String[] args) {
+		// Impressão da mensagem
+		System.out.println("Hello world!");
+	}
+}
